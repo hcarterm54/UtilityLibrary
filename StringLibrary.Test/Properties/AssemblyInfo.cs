ae1129d390943b4e28efab8fe297b66f475af667
Carter Mitchell <cartmit@cdw.com>
2018-03-09 15:48:12 -0600

    Add string class library and test project

---
+++ StringLibrary.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringLibrary.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CDW")]
+[assembly: AssemblyProduct("StringLibrary.Test")]
+[assembly: AssemblyCopyright("Copyright © CDW 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("397a9c31-7c2e-425c-958c-31d8d043e479")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StringLibrary.Test/StringLibraryTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringLibrary.Test
+{
+    [TestClass]
+    public class StringLibraryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
